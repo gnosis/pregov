af58b3aa85154c669b569fc25e762eb90450ad29
ECWireless <Elliott@valtechcreative.com>
2020-10-30 12:49:49 -0600

    Move skins import to helper file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import './helpers/skins';
 import App from "./App";
 import ApolloClient, { InMemoryCache } from 'apollo-boost'
 // import { ApolloProvider } from '@apollo/react-hooks';
@@ -10,19 +11,6 @@ const client = new ApolloClient({
   uri: "https://api.thegraph.com/subgraphs/name/protofire/omen",
   cache: new InMemoryCache()
 });
-
-const requireFile = require.context(
-  '@snapshot-labs/snapshot-spaces/skins/',
-  true,
-  /[\w-]+\.scss$/
-);
-
-requireFile.keys().map(file => requireFile(file));
-
-requireFile
-  .keys()
-  .map(file => file.replace('./', '').replace('.scss', ''));
-
 
 ReactDOM.render(
   <ApolloProvider client={client}>
